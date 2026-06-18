a1006d8e764d735c00d8ea1ae0c3731ee4c5566f
Kaykjunior <kaykjunior855@gmail.com>
2026-06-18 11:44:20 -0300

    update listem port from .env

--- back/src/main.ts
+++ back/src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
 
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(3000);
+  await app.listen(port);
   logger.log(`🚀 Aplicação rodando em: http://localhost:${port}`);
 }
 
